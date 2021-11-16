679365143bc8175d713d03dc69a991d543eb3d9b
cartoonvora <toon_31@hotmail.com>
2021-11-16 02:19:18 -0500

    start complete

--- app.js
+++ app.js
@@ -2,31 +2,13 @@ let cards = document.querySelectorAll(".card")
 let noCards = document.querySelectorAll(".no-card")
 let ansCard = document.querySelectorAll('.ans')
 
-// console.log(cards)
-// suffleCard()
-
-// function suffleCard(){
-//     cards.forEach(card =>{
-//     if (!card.classList.contains("no-card")){
-//         // cards.filter(cards!== noCards)
-//         console.log(card)
-//         let randomCard = Math.floor(Math.random() * 20)
-//         card.style.order = randomCard
-//         }
-//     })
-// }
-// console.log(cards)
-
-// console.log(noCards)
-
-// cards.filter(cards!== noCards)
-
 let randomCArray = [
     "ans1","ans2","ans3","ans4","ans5",
     "ans6","ans7","ans8","ans9","ans10",
     "ans1","ans2","ans3","ans4","ans5",
     "ans6","ans7","ans8","ans9","ans10"]
 
+//shuffle card 
 function shuffle(array) {
     let currentIndex = array.length, randomIndex;
   
@@ -54,54 +36,70 @@ let newCards = card.src
 let selectCard = []
 let matchedCard = []
 
+// win
 function win(){ 
     cards.classList === '.card x'
-    console.log("win!")
+    // console.log("win!")
 }
 
 
-function clickC() {
-    for(let i = 0 ; i < cards.length ; i++){
-        cards[i].addEventListener("click", () => {
-            cards[i].classList.remove("x")
-            selectCard.push(cards[i])
-            if(selectCard.length === 2){
-                setTimeout(checkForMatch,500)
-            }
-            // console.log(selectCard)
-        })
-        function checkForMatch(card1,card2){
-            let cardF = selectCard[0]
-            let cardS = selectCard[1]
-            console.log(cardF.children[1].children[0],cardS.children[1].children[0])
+// start
+function startGame(){
+    document.querySelector('.overlay-playbutton').addEventListener("click",()=>{
+        document.querySelector('.overlay-start').classList.remove("visible")
 
-            if( cardF.children[1].children[0].src === cardS.children[1].children[0].src){
-                matchedCard.push(card1, card2)
-                if(matchedCard.length === 20){
-                    win()
-                    clearInterval(playerTimer)
-                }
-                console.log('match')
-                selectCard = []
-                // console.log(matchedCard)
+// timer
+        let playerTimer = setInterval(timeRun, 1000)
+        function timeRun(){
+        let time = document.querySelector('.timer').innerText 
+        time++
+        document.querySelector('.timer').innerText = time
 
-            } else {
-                cardF.classList.add("x")
-                cardS.classList.add("x")
-                console.log('no match')
-                selectCard = []
-
-            } 
         }
-        
-    }
-}
-clickC()
+        clickC()
 
-let playerTimer = setInterval(timeRun, 1000)
-function timeRun(){
-    let time = document.querySelector('.timer').innerText 
-    time++
-    document.querySelector('.timer').innerText = time
+// click
+        function clickC() {
+            for(let i = 0 ; i < cards.length ; i++){
+                cards[i].addEventListener("click", () => {
+                    cards[i].classList.remove("x")
+                    selectCard.push(cards[i])
+                    if(selectCard.length === 2){
+                        setTimeout(checkForMatch,500)
+                    }
+                    // console.log(selectCard)
+                })
+
+// match
+                function checkForMatch(card1,card2){
+                    let cardF = selectCard[0]
+                    let cardS = selectCard[1]
+                    console.log(cardF.children[1].children[0],cardS.children[1].children[0])
+        
+                    if( cardF.children[1].children[0].src === cardS.children[1].children[0].src){
+                        matchedCard.push(card1, card2)
+                        if(matchedCard.length === 20){
+                            win()
+                            clearInterval(playerTimer)
+                        }
+                        console.log('match')
+                        selectCard = []
+                        // console.log(matchedCard)
+        
+                    } else {
+                        cardF.classList.add("x")
+                        cardS.classList.add("x")
+                        console.log('no match')
+                        selectCard = []
+        
+                    } 
+                }
+                
+            }
+        }
+
+    })    
 }
+startGame()
+
 
